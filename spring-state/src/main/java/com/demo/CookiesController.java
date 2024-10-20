4deac34a086a3c518226845b110b59398545536e
DenisKP <denis.buserski@gmail.com>
2024-10-20 13:28:53 +0300

    Add spring-state

---
+++ spring-state/src/main/java/com/demo/CookiesController.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CookiesController {
+    private static final String LAND_COOKIE_NAME = "lang";
+
+    @GetMapping("/cookies")
+    public String cookies(Model model, @CookieValue(name = LAND_COOKIE_NAME, defaultValue = "en") String lang) {
+        model.addAttribute("lang", lang);
+        return "cookies";
+    }
+
+    @PostMapping("/cookies")
+    public String cookies(
+            HttpServletResponse response,
+            @RequestParam("language") String language) {
+        Cookie cookie = new Cookie(LAND_COOKIE_NAME, language);
+        response.addCookie(cookie);
+
+        return "redirect:/cookies";
+    }
+}
